c54e44bfce63345bdd780ba2a32044b8b0bdcd6d
User <User@DESKTOP-5IPJB2T>
2023-08-23 15:35:40 +0300

    Added CodeConfirmation

---
+++ Controllers/ConfirmationController.cs
@@ -0,0 +1,59 @@
+﻿using FactoryAPI.Models;
+using FactoryAPI.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace FactoryAPI.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class ConfirmationController : Controller
+    {
+        private readonly ApplicationContext _context;
+        public ConfirmationController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetConfirmationCode([FromHeader] string Authorization)
+        {
+            int id = TokenDecoder.GetIdFromToken(Authorization);
+            var user = _context.User.Find(id);
+
+            if (user is null)
+            {
+                return BadRequest("Wrong Access Token");
+            }
+
+            user.ConfirmationCode = CodeGenerator.GenerateNumericCode();
+            _context.SaveChanges();
+
+            return Ok(user.ConfirmationCode);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult PostConfirmationCode([FromHeader] string Authorization, [FromBody] int code)
+        {
+            int id = TokenDecoder.GetIdFromToken(Authorization);
+            var user = _context.User.Find(id);
+            if (user is null)
+            {
+                return BadRequest("Wrong Access Token");
+            }
+
+            if (user.ConfirmationCode == code)
+            {
+                return Ok("Подтверждено успешно.");
+            }
+            else
+            {
+                return BadRequest("Код не совпадает.");
+            }
+        }
+    }
+}
---
+++ Utilities/TokenDecoder.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+
+namespace FactoryAPI.Utilities
+{
+    static public class TokenDecoder
+    {
+        static public ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = AuthOptions.ISSUER,
+                ValidateAudience = true,
+                ValidAudience = AuthOptions.AUDIENCE,
+                ValidateLifetime = false,
+                IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                ValidateIssuerSigningKey = true,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
+
+        }
+        public static JwtSecurityToken ReadToken(string token)
+        {
+            var validations = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = AuthOptions.ISSUER,
+                ValidateAudience = true,
+                ValidAudience = AuthOptions.AUDIENCE,
+                ValidateLifetime = false,
+                IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                ValidateIssuerSigningKey = true,
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            tokenHandler.ValidateToken(token, validations, out SecurityToken securityToken);
+
+            if (securityToken is null)
+            {
+                throw new SecurityTokenException("Invalid token");
+            }
+
+            return (JwtSecurityToken)securityToken;
+        }
+        public static int GetIdFromToken(string token)
+        {
+            if (token.Contains("bearer ") || token.Contains("Bearer "))
+            {
+                token = token.Remove(0, 7);
+            }
+            var securityToken = new JwtSecurityToken(token) ?? throw new SecurityTokenException("Invalid token");
+            var claims = securityToken.Claims ?? throw new SecurityTokenException("Invalid token");
+            string id = claims.First(claim => claim.Type == "User id:").Value;
+            return Convert.ToInt32(id);
+        }
+    }
+}
